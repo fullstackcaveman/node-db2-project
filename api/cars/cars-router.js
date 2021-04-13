efd6b9a0c557c1323c75166c1e55d61b0e9e59fa
Chris Burton <chris@fullstackcaveman.com>
2021-04-13 16:36:28 -0600

    Some middleware work

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const db = require('../../data/db-config');
-const { checkCarId } = require('./cars-middleware');
+const {
+	checkCarId,
+	checkCarPayload,
+	checkVinNumberUnique,
+} = require('./cars-middleware');
 const Cars = require('./cars-model');
 
 const router = express.Router();
@@ -26,13 +30,19 @@ router.get('/:id', checkCarId, async (req, res) => {
 	res.json(req.car);
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', checkCarPayload, async (req, res, next) => {
 	try {
 		const newCar = await Cars.create(req.body);
 		res.status(201).json(newCar);
 	} catch (err) {
 		next(err);
 	}
+});
+
+router.use((err, req, res, next) => {
+	res
+		.status(err.status || 500)
+		.json({ message: err.message, stack: err.stack });
 });
 
 module.exports = router;
