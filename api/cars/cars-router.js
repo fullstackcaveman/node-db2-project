4914d7e78587727eb9178b246f86d30b4a992e95
Chris Burton <chris@fullstackcaveman.com>
2021-04-13 13:20:01 -0600

    TAble built and seeded - get route working in Postman

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,24 @@
-// DO YOUR MAGIC
+const express = require('express');
+const db = require('../../data/db-config');
+const Cars = require('./cars-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+	try {
+		const data = await Cars.getAll();
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+
+	db('cars')
+		.then((cars) => {
+			res.json(cars);
+		})
+		.catch(() => {
+			res.status(500).json({ message: 'Failed to retrieve cars' });
+		});
+});
+
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,15 @@
 // STRETCH
+exports.seed = function (knex) {
+	return knex('cars')
+		.truncate()
+		.then(function () {
+			return knex('cars').insert([
+				{
+					vin: 18436572,
+					make: 'Chevrolet',
+					model: 'Corvette',
+					mileage: 2000,
+				},
+			]);
+		});
+};
